ffd36a015adab2a8b6da4f3f69f1957316cd3e9f
Joseph Sorenson <joseph.sorenson@zenuity.com>
2019-11-08 13:52:48 -0500

    Builds and runs (maybe memory leak?)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-
 #include <EASTL/vector.h>
+#include <EASTL/fixed_vector.h>
+
+void* operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+    return malloc(size);
+}
+
+void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* pName, int flags, unsigned debugFlags, const char* file, int line)
+{
+    return malloc(size);
+}
 
 int main()
 {
@@ -10,10 +20,19 @@ int main()
         vec.push_back(i);
     }
 
-    std::cout << "Hello, World!" << std::endl;
-
     for (int i = 0; i < 5; ++i)
     {
         std::cout << vec[i] << std::endl;
+    }
+
+    eastl::fixed_vector<int, 5> fixedVector;
+    for (int i = 0; i < 5; ++i)
+    {
+        fixedVector.push_back(i);
+    }
+
+    for (int i = 0; i < 5; ++i)
+    {
+        std::cout << fixedVector[i] << std::endl;
     }
 }
